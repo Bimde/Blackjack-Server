9ea1a9c2d6f7f801deca320b91c6a74b8fce566f
BarbaraGuo <barbarag246@gmail.com>
2015-12-02 17:47:42 -0500

    Leaderboard

--- src/gameplay/Leaderboard.java
+++ src/gameplay/Leaderboard.java
@@ -1,5 +1,80 @@
 package gameplay;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Leaderboard {
+	
+	private static PrintWriter fileOut;
+	private static Scanner inFile;
+	private static int[] highScores;
+	private static String[] topPlayers;
+	private static String name;
+	private static int score;
+	
+	
+	Leaderboard(String name, int score) throws IOException
+	{
+		fileOut = new PrintWriter(new FileWriter("topScores.txt"));
+		inFile = new Scanner(new File("topScores.txt"));
+		highScores = new int[10];
+		topPlayers = new String[10];
+		this.name = name;
+		this.score = score;
+		
+		readIn();
+		writeOut();
+	}
+
+	public static void readIn()
+	{
+		int line = 0;
+		
+		while(inFile.hasNext())
+		{
+			highScores[line] = inFile.nextInt();
+			topPlayers[line] = inFile.nextLine();
+		}
+		inFile.close();
+		
+		for (int playerScore = highScores.length - 1; playerScore >= 0; playerScore--)
+		{
+			if (highScores[playerScore] > playerScore
+					&& playerScore != (highScores.length - 1))
+			{
+				for (int next = highScores.length - 1; next > playerScore + 1; next--)
+				{
+					highScores[next] = highScores[next - 1];
+					topPlayers[next] = topPlayers[next - 1];
+				}
+				highScores[playerScore] = score;
+				topPlayers[playerScore] = name;
+
+			}
+			// Handles last index situation
+			else if (highScores[playerScore] <= score && playerScore == 0)
+			{
+				for (int next = highScores.length - 1; next > 0; next--)
+				{
+					highScores[next - 1] = highScores[next];
+					topPlayers[next - 1] = topPlayers[next];
+				}
+				highScores[playerScore] = score;
+				topPlayers[playerScore] = name;
+			}
+		}
+	}
+	
+	public static void writeOut()
+	{
+		for (int player = 0; player < topPlayers.length; player++)
+		{
+			fileOut.println(topPlayers[player] + " " + highScores[player]);
+		}
+		fileOut.close();
+	}
 
 }
