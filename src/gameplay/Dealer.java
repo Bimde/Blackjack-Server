30beef2447e8a5563d9745dc503ddc6c5f57f682
Bimesh De Silva <bimeshde@gmail.com>
2015-12-02 12:12:03 -0500

    Added client list to server

--- src/gameplay/Dealer.java
+++ src/gameplay/Dealer.java
@@ -1,9 +1,7 @@
 package gameplay;
 
-import java.util.ArrayList;
-
-import connection.Client;
 import connection.Server;
+import utilities.ClientList;
 
 /**
  * Handles the actual gameplay, i.e. which player's turn is it, giving the
@@ -15,12 +13,12 @@ public class Dealer {
 	public static final String[] RANKS = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 	public static final int NUMBER_OF_DECKS = 4;
 	private Deck deck;
-	private ArrayList<Client> clients;
+	private ClientList clients;
 	private Server server;
 	private int currentPlayer;
 	public int[] playerCoins;
 
-	public Dealer(Server server, ArrayList<Client> clients) {
+	public Dealer(Server server, ClientList clients) {
 		this.deck = new Deck(NUMBER_OF_DECKS);
 		this.currentPlayer = 0;
 		this.server = server;
