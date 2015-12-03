29e23eca2b22768627d1ff05e471b61ec67aca86
Bimesh De Silva <bimeshde@gmail.com>
2015-12-03 13:59:50 -0500

    Implemented dynamic player number generation

--- src/utilities/ClientList.java
+++ src/utilities/ClientList.java
@@ -4,36 +4,51 @@ import connection.Client;
 
 public class ClientList {
 	private ClientNode head, tail;
-	private int playerNo;
 
 	/**
-	 * Creates a new linked list containing Client objects Starts the client
-	 * numbers from 1
+	 * Creates a new linked list containing Client objects
 	 */
 	public ClientList() {
-		this.playerNo = 1;
 	}
 
 	/**
-	 * Add a new Client to the list
+	 * Adds specified client to list
 	 * 
 	 * @param temp
+	 *            Client to add to list
 	 */
 	public void add(Client temp) {
-		ClientNode add = new ClientNode(this.playerNo, temp);
+		ClientNode add = new ClientNode(this.findEmptyPlayerNo(), temp);
 		if (this.head == null)
 			this.head = this.tail = add;
 		else {
 			this.tail.setNext(add);
 			this.tail = add;
 		}
-		this.playerNo++;
 	}
 
 	/**
-	 * Counts the number of Client objects in the list
+	 * Finds the next available player number
 	 * 
-	 * @return The number of clients in the list
+	 * @return Next available player number
+	 */
+	private int findEmptyPlayerNo() {
+		ClientNode temp = this.head;
+		int lastNo = 0;
+		while (temp != null) {
+			int nextNo = temp.getPlayerNo();
+			if (nextNo > lastNo + 1)
+				return lastNo + 1;
+			lastNo = nextNo;
+			temp = temp.getNext();
+		}
+		return lastNo + 1;
+	}
+
+	/**
+	 * Counts number of Client objects in list
+	 * 
+	 * @return Number of clients in list
 	 */
 	public int size() {
 		ClientNode temp = this.head;
@@ -49,7 +64,7 @@ public class ClientList {
 	 * 
 	 * @param playerNo
 	 *            The number associated with the desired client
-	 * @return Client associated with specified player number, or -1 or not
+	 * @return Client associated with specified player number, or null or not
 	 *         found
 	 */
 	public Client get(int playerNo) {
